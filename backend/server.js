4f14f0ff8966f0ec9458ba0b1716345ffcc7c577
Perry_Nguyen <nguyenperry99@gmail.com>
2024-12-16 13:24:27 -0600

    setting up the server

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const projectRoutes = require('./Routes/projects')
+// express app
+
+const app = express()
+
+//middleware this will show us what is being use aka get post put and del
+app.use(express.json())
+app.use((req,res,next)=>{
+    console.log(req.path, req.method)
+    next()
+})
+
+//Routes
+app.use('/api/projects',projectRoutes)
+
+//connect to db
+
+mongoose.connect(process.env.MONG_URI) // this is a promise
+    .then(() => {
+
+        //listening to port 4000
+        app.listen(process.env.PORT, () =>{
+            console.log("connecting to DB & listening to",process.env.PORT)
+        })
+
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+
